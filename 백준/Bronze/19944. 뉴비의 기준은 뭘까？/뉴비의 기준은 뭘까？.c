364442bb8ce3ad989007cbdddc9a54dd9fdfd403
orientaition <49131957+orientaition@users.noreply.github.com>
2022-07-31 19:33:28 +0900

    [Bronze IV] Title: 뉴비의 기준은 뭘까?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/19944. 뉴비의 기준은 뭘까？/뉴비의 기준은 뭘까？.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int n, m;
+    scanf("%d %d", &n, &m);
+    if(m <= 2){
+        printf("NEWBIE!");
+    }
+    else if(n < m){
+        printf("TLE!");
+    }
+    else{
+        printf("OLDBIE!");
+    }    
+    return 0;
+}
